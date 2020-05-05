cec68a67410e46fd281cde041a87595eb4c2324d
sholmas <stian@holmas.com>
2020-05-05 09:17:03 +0200

    Updating scripts.

--- updateDev.sh
+++ updateDev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -u main.py 19070 bgw-dev >> logs/updateDev.log 2> logs/updateDev.err &
+python3 -u main.py http://ssb4.nt.ntnu.no:19070 bgw-dev >> logs/updateDev.log 2> logs/updateDev.err &
