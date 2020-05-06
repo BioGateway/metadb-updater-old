eeebfd01e7d462cf8e45bd593e8fa0f04b6e95c9
Stian Holmas <stian@holmas.com>
2020-05-06 18:58:17 +0200

    Removing update scripts.

--- updateDev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 -u main.py ssb4.nt.ntnu.no:20030 bgw-dev >> logs/updateDev.log 2> logs/updateDev.err &
--- updateProd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 -u main.py ssb4.nt.ntnu.no:20030 bgw-cache >> logs/updateProd.log 2> logs/updateProd.err &
